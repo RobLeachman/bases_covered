c5951b7cb477fb0f86f03df7284489536aa5b2a5
peter <pkeoman17@gmail.com>
2015-10-24 09:32:21 -0700

    ui stuff

--- src/base_cover.py
+++ src/base_cover.py
@@ -13,4 +13,7 @@ print("Commands:")
 print("show_wk <month> <start day of the week(Sunday)>  ---Displays the current week")
 print("show_next <month> <start day of the week(Sunday)> ---Similar but display next week")
 print("assign <Person> <date>                            ---Assign a person")
+print("school <school name>                              ---Show school's events")
+print("quit                                              ---Quit")
+
 
